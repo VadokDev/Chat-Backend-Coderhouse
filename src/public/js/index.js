2090b5798d0d5f84decb615fdb4e3d58fb9c2a06
VadokDev <gonzalo.fdez06@gmail.com>
2023-08-31 20:50:06 -0400

    first commit

---
+++ src/public/js/index.js
@@ -0,0 +1,35 @@
+const socket = io();
+
+let usuario = '';
+
+const caja = document.getElementById('caja');
+const contenido = document.getElementById('contenido');
+
+caja.addEventListener('change', (e) => {
+  socket.emit('mensaje', {
+    user: usuario,
+    mensaje: e.target.value,
+  });
+});
+
+Swal.fire({
+  title: 'Ingresa tu nombre',
+  input: 'text',
+  inputAttributes: {
+    autocapitalize: 'off',
+  },
+  confirmButtonText: 'Ingresar',
+  showLoaderOnConfirm: true,
+}).then((result) => {
+  usuario = result.value;
+});
+
+// [ {user: "usuario", mensaje: "mensaje"} ]
+
+socket.on('nuevo_mensaje', (data) => {
+  const mensajes = data.map(({ user, mensaje }) => {
+    return `<p>${user} dijo: ${mensaje}</p>`;
+  });
+
+  contenido.innerHTML = mensajes.join('');
+});
